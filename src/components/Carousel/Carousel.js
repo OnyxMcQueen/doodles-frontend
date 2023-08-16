8ed4f8debdc646c12d3986de100607c74cc209c4
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-16 14:30:21 -0400

    Changed class to className

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -16,13 +16,13 @@ function Carousel() {
       <img className="d-block w-100 slider-image" src="https://static.vecteezy.com/system/resources/previews/023/863/498/original/abstract-doodle-art-pattern-doodle-wallpaper-vector.jpg" alt="Third slide" />
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="sr-only">Next</span>
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span className="sr-only">Next</span>
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
   </a>
 </div>
   )
