7ef3d596154210c34ac8690bf89b62222d59e186
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-16 15:56:38 -0400

    Created a functioning Navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import './Nav.css';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className='navbar'>
+      <Link className='logo' to='/'>
+        <img className='navbar-img' src='https://media.istockphoto.com/id/458716003/photo/we-the-youth-by-keith-haring.jpg?s=612x612&w=0&k=20&c=v-Zs8OChKrpIva2OFQ2r-lCFNWKVcOfWWmFVAx4OL6o=' alt='Logo for website'/>
+      </Link>
+
+      <div className='menu'>
+      <Link to='/doodles'><h3>View All Doodles</h3></Link>    
+      <Link to='/new-doodle'><h3>Create a Doodle</h3></Link>
+      <Link to='/about-me'><h3>About Me</h3></Link>
+      </div>
+    </nav>
   )
 }
 
