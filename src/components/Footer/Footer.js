e9c308789d0be4b9079e06807852f7a1d32efbc7
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-16 17:15:22 -0400

    Created a Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <p>© Pencil Palette 2023. All rights left because right is too mainstream.</p>
+    </div>
+  )
+}
+
+export default Footer
