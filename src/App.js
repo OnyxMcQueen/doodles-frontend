eede162c98949c23802e6e4466e60de904a6d0fd
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-16 04:50:38 -0400

    Created Nav component/Currently working on Home Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+
 function App() {
-  return ({});
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
