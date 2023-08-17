361e2d1d1b05f0cfb9b0357883efca449f23f5db
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-16 20:10:27 -0400

    Added lyket Provider component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from '@lyket/react';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider apiKey='pt_22e3b799a457363b141082004cd5fd'>
+        <App />
+    </Provider>
 );
