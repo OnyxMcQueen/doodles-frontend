2bceb60f1eaf1875f48199cfdb09d17ccd87f460
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-21 03:31:22 -0400

    Created Spinner w/ CSS for loading

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Spinner.css';
+
+function Spinner() {
+  return (
+    <div className='spinner-container'>
+        <div class="custom-loader"></div>
+    </div>
+  )
+}
+
+export default Spinner
